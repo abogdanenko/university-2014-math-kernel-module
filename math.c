e11ce27fc90024e69618af0edc764af367464685
Alexey Bogdanenko <alexey@bogdanenko.com>
2014-05-05 02:21:12 +0400

    Add file operation templates, create character device

--- math.c
+++ math.c
@@ -3,18 +3,71 @@
 #include <linux/fs.h>
 #include <linux/sched.h>
 #include <linux/uaccess.h>
+#include <linux/cdev.h>
 
 #include "math.h"
 
+struct math_device_t
+{
+    struct cdev cdev;
+    dev_t num;
+} math_device;
+
+int ret; // return code
+
+int fop_open(struct inode* ip, struct file* fp)
+{
+    pr_info("math: opened device\n");
+    return 0;
+}
+
+int fop_release(struct inode* ip, struct file* fp)
+{
+    pr_info("math: released device\n");
+    return 0;
+}
+
+long fop_unlocked_ioctl(struct file* fp, unsigned int cmd, unsigned long arg)
+{
+    pr_info("math: ioctl\n");
+    return 0;
+}
+
+struct file_operations fops = 
+{
+    .owner = THIS_MODULE,
+    .open = fop_open,
+    .release = fop_release,
+    .unlocked_ioctl = fop_unlocked_ioctl
+};
+
 static int math_init(void)
 {
-    pr_info("Module math was loaded\n");
+    ret = alloc_chrdev_region(&(math_device.num), 0, 1, "math");
+
+    if (ret < 0)
+    {
+        pr_err("math: failed to allocate device number\n");
+        return ret;
+    }
+
+    cdev_init(&(math_device.cdev), &fops);
+    ret = cdev_add(&(math_device.cdev), math_device.num, 1);
+    
+    if (ret < 0)
+    {
+        pr_err("math: unable to add character device\n");
+        return ret;
+    }
+
+    pr_info("math: loaded module\n");
     return 0;
 }
 
 static void math_exit(void)
 {
-    pr_info("Module math was unloaded\n");
+    unregister_chrdev_region(math_device.num, 1);
+    pr_info("math: unloaded module\n");
 }
 
 module_init(math_init);
