30435340e4bf148f0852c611a170d6e01629d5d3
Alexey Bogdanenko <alexey@bogdanenko.com>
2014-05-04 14:32:50 +0400

    Updated author info

--- math.c
+++ math.c
@@ -8,17 +8,18 @@
 
 static int math_init(void)
 {
-	pr_info("Module math was loaded\n");
+    pr_info("Module math was loaded\n");
     return 0;
 }
 
 static void math_exit(void)
 {
-	pr_info("Module math was unloaded\n");
+    pr_info("Module math was unloaded\n");
 }
 
 module_init(math_init);
 module_exit(math_exit);
-MODULE_AUTHOR("Ivan Ivanovich Ivanov IV <iii@ivanovo.su>");
+
+MODULE_AUTHOR("Alexey Bogdanenko <alexey@bogdanenko.com>");
 MODULE_DESCRIPTION("Math module");
 MODULE_LICENSE("GPL");
